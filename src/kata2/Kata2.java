f5459dd88f8d7fe692058c01ae0980a4b3f514a7
JorgeAM1951 <100132639+JorgeAM1951@users.noreply.github.com>
2022-09-28 09:44:21 +0100

    First Version

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -18,7 +18,7 @@ public class Kata2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int [] data = {1,4,1,4,5,6,8,8,4,5,6,2};
+        Integer [] data = {1,4,1,4,5,6,8,8,4,5,6,2};
         
         
         HistogramGenerator histogramGenerator = new HistogramGenerator(data);
@@ -30,4 +30,4 @@ public class Kata2 {
         }
     }
     
-}
+
